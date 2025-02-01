65ca4c37e533718e52189145a55a41a89a58282c
Evaldo Rodrigues <110418142+evaaldo@users.noreply.github.com>
2025-01-31 22:04:06 -0300

    Delete UsersDocker directory

--- UsersDocker/Entites/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UsersDocker.Entites.Models
-{
-    public class User
-    {
-        public int ID { get; set; }
-        public string? Name { get; set; }
-        public string? Password { get; set; }
-    }
-}
